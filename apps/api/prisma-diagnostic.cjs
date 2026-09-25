375ce350055b07e9263ad6d3dc71e595e30f8d6d
BigJames3 <traorenignaman@outlook.fr>
2026-09-25 03:46:19 +0000

    fix(ci): format prisma diagnostic script

--- apps/api/prisma-diagnostic.cjs
+++ apps/api/prisma-diagnostic.cjs
@@ -1,17 +1,17 @@
-﻿const { PrismaClient, Prisma } = require("@prisma/client");
+﻿const { PrismaClient, Prisma } = require('@prisma/client');
 
 const p = new PrismaClient();
 
-console.log("PrismaClient:", !!PrismaClient);
-console.log("Prisma:", !!Prisma);
-console.log("$transaction:", typeof p.$transaction);
+console.log('PrismaClient:', !!PrismaClient);
+console.log('Prisma:', !!Prisma);
+console.log('$transaction:', typeof p.$transaction);
 
 console.log(
-  "models:",
+  'models:',
   Object.keys(p)
-    .filter((k) => !k.startsWith("_"))
+    .filter((k) => !k.startsWith('_'))
     .sort()
-    .join(", ")
+    .join(', ')
 );
 
 p.$disconnect();
